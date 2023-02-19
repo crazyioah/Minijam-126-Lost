5095278cc1b5ccae4c95ac023d3e7cee9f1a53bb
CrazyEyeOah <noahcappelle@hotmail.com>
2023-02-18 20:16:33 -0600

    more

--- Lost minijam/=Character.gd
+++ Lost minijam/=Character.gd
@@ -9,17 +9,17 @@ func get_input():
 	if Input.is_action_pressed("move_right"):
 		velocity.x += 1
 		animated_sprite.play("walk")
-	if Input.is_action_pressed("move_left"):
+		animated_sprite.scale = Vector2(1, 1)
+	elif Input.is_action_pressed("move_left"):
 		velocity.x -= 1
 		animated_sprite.play("walk")
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1
-		animated_sprite.play("walk")
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1
-		animated_sprite.play("walk")
+		animated_sprite.scale = Vector2(-1, 1)
 	else: 
 		animated_sprite.stop()
+	if Input.is_action_pressed("move_up"):
+		velocity.y -= 1
+	elif Input.is_action_pressed("move_down"):
+		velocity.y += 1
 	velocity  = velocity.normalized() * speed
 func _physics_process(delta):
 	get_input()
