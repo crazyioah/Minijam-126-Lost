484315545ea09d561d1faa0326179e1b5b57597a
CrazyEyeOah <noahcappelle@hotmail.com>
2023-02-19 13:43:56 -0600

    fdg

--- Lost minijam/random_expansion.gd
+++ Lost minijam/random_expansion.gd
@@ -1,13 +1,21 @@
 extends Node2D
 
+onready var timer = $Timer
 onready var tilemap = $Tiles/Raft
 var map_size = Vector2(20, 20)
 
-
-
+func _ready():
+	timer.start()
 
 func _on_Timer_timeout():
-	for x in range(map_size.x):
-		for y in range(map_size.y):
-			if randf() < 0.2:
-				tilemap.set_cell(x, y, randi() % tilemap.tile_set.tile_count)
+	var x = rand_range(0, map_size.x - 1)
+	var y = rand_range(0, map_size.y - 1)
+	var tile_path = get_tile_path(x, y)
+	print("Tile at position (" + str(x) + ", " + str(y) + ") has path: " + tile_path)
+	timer.start()
+
+func get_tile_path(x: int, y: int) -> String:
+	var tileset = tilemap.tile_set
+	var tile_id = tilemap.get_cell(x, y)
+	var tile_info = tileset.get_tile_info(tile_id)
+	return tile_info.texture.get_path()
